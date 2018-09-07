f5ef021928401611d2842da7dce1bcd01628953b
Surah <jiaying1@andrew.cmu.edu>
2018-09-06 23:48:34 -0700

    Delete 175--Combine Two Tables.py

--- SQL/Python/175--Combine Two Tables.py
+++
@@ -1,6 +0,0 @@
-#Python Solution
-#Using Pandas
-import pandas as pd
-results=pd.merge(Person[['FirstName','LastName']],Address[['City','State']],
-	             left_on='PersonId',right_on='PersonId',how=left)
-print results
